74da3f966c78985f07a0fa5c7a39ec453d5fd65b
Lucas Koval <lucaskovaldev@gmail.com>
2022-03-24 18:40:19 -0300

    Starting File management challenge

---
+++ 2-File-Management/ManejoDeArchivos.js
@@ -0,0 +1,17 @@
+class Container {
+  constructor(name) {
+    this.name = name
+  }
+
+  save(object) {}
+
+  getById(number) {}
+
+  getAll() {}
+
+  deleteById(number) {}
+
+  deleteAll() {}
+}
+
+const container = new Container('fileName')
