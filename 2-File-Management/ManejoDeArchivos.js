efd57e75567cd0ff70e715e1720214fcb9944f74
Lucas Koval <lucaskovaldev@gmail.com>
2022-03-29 18:07:08 -0300

    Second challenge - first method added

--- 2-File-Management/ManejoDeArchivos.js
+++ 2-File-Management/ManejoDeArchivos.js
@@ -10,17 +10,46 @@ class Container {
 
   async save(object) {
     object.id = prevId + 1
-    this.array.push(object)
-    await fs.promises.writeFile(this.path, JSON.stringify(this.object))
     prevId++
+    this.array.push(object)
+    await fs.promises.writeFile(this.path, JSON.stringify(this.array))
+    console.log('%c⧭ id', 'color: #00ff03;', object.id)
+    return object.id
   }
+
+  async getById(number) {
+    const fileData = await fs.promises.readFile(this.path, 'utf-8')
+    console.log('%c⧭ fileData', 'color: #ff6c61;', fileData)
+    const found = fileData.find((object) => object.id === 2)
+    console.log('%c⧭ found', 'color: #ff6c61;', found)
+  }
+
+  async getAll() {}
+
+  async deleteById(number) {}
+
+  async deleteAll() {}
 }
 
-const container = new Container()
+const container = new Container('test.txt')
 
-const testObject = {
+const testObject_1 = {
   name: 'Test_Name',
-  size: 50,
+  size: 7,
 }
 
-container.save(testObject)
+const testObject_2 = {
+  name: 'Test_Name',
+  size: 10,
+}
+
+const testObject_3 = {
+  name: 'Test_Name',
+  size: 21,
+}
+
+container.save(testObject_1)
+container.save(testObject_2)
+container.save(testObject_3)
+
+container.getById(2)
